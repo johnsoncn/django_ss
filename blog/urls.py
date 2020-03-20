5f1bc38060529b3c2e5f727d1c976e525ba0ecda
johnsonuk <johnsonuk@163.com>
2020-03-20 17:21:11 +0800

    include优化urls

---
+++ blog/urls.py
@@ -0,0 +1,20 @@
+"""
+
+这是把工程文件里的 urls.py 模块直接复制过来
+"""
+
+from django.conf.urls import url
+from blog.views import index1,index2
+
+urlpatterns = [
+    url(r'test1/', index1),
+    url(r'test2/', index2),
+]
+
+
+"""
+python manage.py runserver
+
+http://127.0.0.1:8000/myAPP/test1/
+http://127.0.0.1:8000/myAPP/test2/
+"""
